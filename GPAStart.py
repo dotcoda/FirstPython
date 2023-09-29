8d2a702a72bbdd65a3f7e11881c98c7564b4e884
dotcoda <146485915+dotcoda@users.noreply.github.com>
2023-09-29 15:38:49 -0500

    Add files via upload

---
+++ GPAStart.py
@@ -0,0 +1,27 @@
+gpaOne=4
+gpaTwo=3
+gpaThree=2
+
+creditsOne=6
+creditsTwo=5
+creditsThree=1
+
+totalCredits = creditsOne+creditsTwo+creditsThree
+
+weightedOne=gpaOne*creditsOne
+weightedTwo=gpaTwo*creditsTwo
+weightedThree=gpaThree*creditsThree
+
+totalWeighted=weightedOne+weightedTwo+weightedThree
+
+finalGPA=round ((totalWeighted/totalCredits), 2)
+
+print (finalGPA)
+
+# launch IDLE and run this file
+# explain comments
+# fix the error
+# add line numbers
+# better print line (concatenate it)
+# round the result
+# re-organize my code
